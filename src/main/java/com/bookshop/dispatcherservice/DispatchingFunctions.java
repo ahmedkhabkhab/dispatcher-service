8c41a2f3ad957940c7e5a07cab3c2900e3576c33
Ahmed Khabkhab <ahmed.khabkhab@gmail.com>
2023-01-17 19:35:50 +0100

    Add label function

--- src/main/java/com/bookshop/dispatcherservice/DispatchingFunctions.java
+++ src/main/java/com/bookshop/dispatcherservice/DispatchingFunctions.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Flux;
 
 import java.util.function.Function;
 
@@ -19,6 +20,14 @@ public class DispatchingFunctions {
             log.info("Order {} is packed", orderAcceptedMessage.orderId());
             return orderAcceptedMessage.orderId();
         };
+    }
+
+    @Bean
+    public Function<Flux<Long>, Flux<OrderDispatchedMessage>> label() {
+        return orderFlux -> orderFlux.map(orderId -> {
+            log.info("Order {} is labeled", orderId);
+            return new OrderDispatchedMessage(orderId);
+        });
     }
 
 }
---
+++ src/main/java/com/bookshop/dispatcherservice/OrderDispatchedMessage.java
@@ -0,0 +1,4 @@
+package com.bookshop.dispatcherservice;
+
+public record OrderDispatchedMessage(Long orderId) {
+}
